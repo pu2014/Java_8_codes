d43980a8d3b91a325848b083f69bedc046f33056
pu2014 <whutpbj@163.com>
2020-04-30 11:07:06 +0800

    深拷贝和浅拷贝 clone()

--- src/com/pu/jdk/HowToUseClone.java
+++ src/com/pu/jdk/HowToUseClone.java
@@ -2,6 +2,9 @@ package com.pu.jdk;
 
 /**
  * clone 浅拷贝，对象必须实现cloneable接口。拷贝后对象的地址不一样，但是属性的拷贝是对象的引用
+ *      alibaba规范
+ *      说明： 对象的 clone 方法默认是浅拷贝，若想实现深拷贝需要重写 clone 方法实现属性对象的拷贝。
+ * 如果需要深拷贝，必须完全实现属性也重写clone()方法 ，至于彻底深拷贝，几乎是不可能实现的
  */
 public class HowToUseClone  {
     public static void main(String[] args) throws CloneNotSupportedException {
