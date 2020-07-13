7b1013c9aa92edec951ef2f57b859343e6a043bb
pu2014 <whutpbj@163.com>
2020-07-13 20:13:25 +0800

    fail-safe 机制 juc 不强一致性

---
+++ src/com/pu/thread/concurrent_vector/FailSafe.java
@@ -0,0 +1,13 @@
+package com.pu.thread.concurrent_vector;
+
+/**
+ * @DESC
+ * @CREATE BY @Author pbj on @Date 2020/7/13 17:15
+ */
+public class FailSafe {
+    public static void main(String[] args) {
+        /**
+         * JUC 下的包丢失安全失败的，可以在多线程的情况下并发使用，并发修改
+         */
+    }
+}
