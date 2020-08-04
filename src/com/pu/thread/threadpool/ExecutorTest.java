418ca721033c9ba54154ee41afc9e89e90540d38
pu2014 <whutpbj@163.com>
2020-08-04 16:45:58 +0800

    1. 测试shutdown方法

--- src/com/pu/thread/threadpool/ExecutorTest.java
+++ src/com/pu/thread/threadpool/ExecutorTest.java
@@ -1,13 +1,16 @@
 package com.pu.thread.threadpool;
 
+import javax.swing.plaf.synth.SynthColorChooserUI;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @DESC  线程池框架
  * @CREATE BY @Author pbj on @Date 2020/7/14 17:19
  */
 public class ExecutorTest {
+    private static volatile int cnt = 0;
     public static void main(String[] args) {
         /**
          * return new ThreadPoolExecutor(nThreads, nThreads,
@@ -16,7 +19,23 @@ public class ExecutorTest {
          *
          * 特点：核心线程数和最大线程数一致，链表阻塞队列
          */
-        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(100);
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
+        for(int i = 0; i < 100; i++){
+            fixedThreadPool.execute(()->{
+                synchronized (Thread.class){
+                    cnt++;
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(cnt + Thread.currentThread().getName());
+            });
+            if(i == 50){
+                fixedThreadPool.shutdown();
+            }
+        }
 
         /**
          * new ThreadPoolExecutor(1, 1,
